f35ce1268a1a278ee7a056cf9811e71b54a8c298
Katherine Tam <katherinemtam@gmail.com>
2021-06-30 11:48:17 -0700

    add posts table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,8 +1,17 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS posts;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   email TEXT NOT NULL UNIQUE,
   password_hash TEXT NOT NULL,
-  profile_photo_url TEXT NOT NULL 
-)
+  profile_photo_url TEXT 
+);
+
+CREATE TABLE posts (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id BIGINT REFERENCES users(id) NOT NULL,
+  photo_url TEXT NOT NULL,
+  caption TEXT NOT NULL,
+  tags TEXT [] NOT NULL
+);
