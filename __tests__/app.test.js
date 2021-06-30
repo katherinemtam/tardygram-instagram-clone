833f5018d6e5868e8b7d0542bf81ed3f6eb78999
Katherine Tam <katherinemtam@gmail.com>
2021-06-30 11:46:31 -0700

    users verify test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import UserService from '../lib/services/UserServices.js';
 
 describe('auth routes', () => {
   beforeAll(() => {
@@ -37,6 +38,35 @@ describe('auth routes', () => {
       id: '1',
       email: 'test@test.com',
       profilePhotoUrl: 'profilephoto.url'
+    });
+  });
+
+  test('verifies a user is logged in', async() => {
+    const agent = request.agent(app);
+    const user = await UserService.create({
+      email: 'hello@jello.com',
+      password: 'ahoy',
+      profilePhotoUrl: 'jellybean'
+    });
+
+    await agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'hello@jello.com',
+        password: 'ahoy',
+        profilePhotoUrl: 'jellybean'
+      });
+
+    const res = await agent
+      .get('/api/v1/verify');
+
+    expect(res.body).toEqual({
+      id: user.id,
+      email: 'hello@jello.com',
+      passwordHash: expect.any(String),
+      profilePhotoUrl: 'jellybean',
+      iat: expect.any(Number),
+      exp: expect.any(Number)
     });
   });
 });
