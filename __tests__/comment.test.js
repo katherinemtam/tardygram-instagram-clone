04a842e3261aaaf7b3cd6f6bc52682fec9785d86
Katherine Tam <katherinemtam@gmail.com>
2021-06-30 15:03:34 -0700

    comments delete route test

--- __tests__/comment.test.js
+++ __tests__/comment.test.js
@@ -4,11 +4,13 @@ import request from 'supertest';
 import app from '../lib/app.js';
 import UserService from '../lib/services/UserServices.js';
 import Post from '../lib/models/Post.js';
+import Comment from '../lib/models/Comment.js';
 
 const agent = request.agent(app);
 
 describe('post routes', () => {
   let user;
+  let post;
 
   beforeEach(async () => {
     await setup(pool);
@@ -28,17 +30,17 @@ describe('post routes', () => {
         password: 'password',
         profilePhotoUrl: 'profilephoto.url'
       });
-  });
 
-  test('creates a comment via POST', async () => {
     //creates a post
-    const post = await Post.create({
+    post = await Post.create({
       userId: '1',
       photoUrl: 'cute dog picture',
       caption: 'Happy Doggo Day!',
       tags: ['cute', 'doggo', 'pupper']
     });
+  });
 
+  test('creates a comment via POST', async () => {
     const res = await agent
       .post('/api/v1/comments')
       .send({
@@ -53,5 +55,19 @@ describe('post routes', () => {
       postId: post.id,
       comment: 'That\'s a good boy!',
     });
+  });
+
+  test('deletes a comment via DELETE', async () => {
+    const comment = await Comment.create({
+      commentBy: user.id,
+      postId: post.id,
+      comment: 'Look at the face!'
+    });
+
+    const res = await agent
+      .delete(`/api/v1/comments/${comment.id}`)
+      .send(comment);
+
+    expect(res.body).toEqual(comment);
   });
 });
