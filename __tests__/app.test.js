8893a973fba900c376152a3a9507619bbf53ad41
Katherine Tam <katherinemtam@gmail.com>
2021-06-30 08:36:32 -0700

    login post route test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,13 +4,29 @@ import request from 'supertest';
 import app from '../lib/app.js';
 
 describe('auth routes', () => {
-  beforeEach(() => {
+  beforeAll(() => {
     return setup(pool);
   });
 
   test('signs up a user via POST', async () => {
     const res = await request(app)
       .post('/api/v1/auth/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'password',
+        profilePhotoUrl: 'profilephoto.url'
+      });
+
+    expect(res.body).toEqual({
+      id: '1',
+      email: 'test@test.com',
+      profilePhotoUrl: 'profilephoto.url'
+    });
+  });
+
+  test('logs in a user via POST', async() => {
+    const res = await request(app)
+      .post('/api/v1/auth/login')
       .send({
         email: 'test@test.com',
         password: 'password',
